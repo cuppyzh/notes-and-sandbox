af76fc71225e7f1cad1cd1b029b58590711e3f82
Yusuf Aditama / Cuppyzh <yapaditama@gmail.com>
2024-12-28 20:55:13 +0700

    deno(add): add sample code to create firestore document with predefined ID

--- deno/firebase-firestore-sample/main.ts
+++ deno/firebase-firestore-sample/main.ts
@@ -2,9 +2,12 @@ import {
   initializeApp,
 } from "https://www.gstatic.com/firebasejs/11.1.0/firebase-app.js";
 import {
+addDoc,
   collection,
   getDocs,
   getFirestore,
+  doc,
+  setDoc
 } from "https://www.gstatic.com/firebasejs/11.1.0/firebase-firestore.js";
 import {
   getAuth,
@@ -31,7 +34,18 @@ const userCredential = await signInWithEmailAndPassword(
 );
 console.log("User signed in: ", userCredential.user.email);
 
-const testCollection = collection(database, "{collection-name}");
+// Data specific configuration
+const collectionName = "{collection-name}"
+
+const testCollection = collection(database, collectionName);
+
+// Get Docs
 const docs = await getDocs(testCollection);
-const data = docs.docs.map((doc) => ({ id: doc.id, ...doc.data() }));
-console.log("Data: \n" + JSON.stringify(data))
+const getData = docs.docs.map((doc) => ({ id: doc.id, ...doc.data() }));
+console.log("Data: \n" + JSON.stringify(getData))
+
+// Add Docs with Defined Document ID
+const documentId = "reference-id";
+const docRef = doc(database, collectionName, documentId);
+const data = { name: "John Doe", age: 30, email: "john.doe@example.com", };
+await setDoc(docRef, data);
